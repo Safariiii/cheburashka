11e6a8b566ccb370fd28e73a30de5090f495b51a
Safariiii <Safargaleev92@mail.ru>
2023-03-15 18:21:45 +0400

    - add collider to bullet

--- Assets/BulletShooter/BulletShooter.cs
+++ Assets/BulletShooter/BulletShooter.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.Tools;
 
+namespace MoreMountains.InfiniteRunnerEngine {
 public class BulletShooter : MonoBehaviour
 {
 
     [SerializeField] private float bulletSpeed = 10f;
     [SerializeField] private Rigidbody2D bullet;
     [SerializeField] private float timeBetweenShots = 1f;
+    public GameObject Explosion;
     private float bulletDelay = 0.1f;
     private float timeLeft = 0f;
     private bool canShoot = true;
@@ -32,13 +35,25 @@ public class BulletShooter : MonoBehaviour
             }
         }
         if (Input.GetKeyDown(KeyCode.E) && canShoot) {
-            StartCoroutine(ExampleCoroutine());
+            StartCoroutine(FireBullets());
             canShoot = false;
         }
         
      }
 
-     IEnumerator ExampleCoroutine()
+     void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            GameObject explosion = (GameObject)Instantiate(Explosion);
+	        explosion.transform.position = new Vector3(transform.GetComponent<Renderer>().bounds.min.x, transform.GetComponent<Renderer>().bounds.center.y,0);
+	        MMAnimatorExtensions.UpdateAnimatorBoolIfExists(explosion.GetComponent<Animator>(), "Explode", true);
+			// we turn the object inactive so it can be instantiated again 
+	        other.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator FireBullets()
     {
         Fire();
         yield return new WaitForSeconds(bulletDelay);
@@ -47,3 +62,6 @@ public class BulletShooter : MonoBehaviour
         Fire();
     }
 }
+
+}
+
