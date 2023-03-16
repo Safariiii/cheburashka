c992eac89567bdf4080857139872013a5daf6af1
Safariiii <Safargaleev92@mail.ru>
2023-03-16 10:38:42 +0400

    - refactor enemy generator

--- Assets/EnemyGenerator.cs
+++ Assets/EnemyGenerator.cs
@@ -7,28 +7,14 @@ public class EnemyGenerator : MonoBehaviour
 
     [SerializeField] private Rigidbody2D enemy;
 
-    void Start() {
-        Rigidbody2D clone = (Rigidbody2D) Instantiate(enemy, new Vector3(), transform.rotation);
-    }
-
     void OnEnable() {
-
         GameObject respawn = GameObject.FindWithTag("Enemy");
-        respawn.transform.position = new Vector3();
-        respawn.transform.rotation = new Quaternion();
-        respawn.SetActive(true);
-        // if (respawn == null) {
-        //     Rigidbody2D clone = (Rigidbody2D) Instantiate(enemy, new Vector3(), transform.rotation);
-        // } else {
-        //     respawn.transform.position = new Vector3();
-        //     respawn.transform.rotation = new Quaternion();
-        //     respawn.SetActive(true);
-        // }
-    }
-
-    void OnDisable() {
-        GameObject respawn = GameObject.FindWithTag("Enemy");
-        respawn.SetActive(false);
-
+        if (respawn == null) {
+            Rigidbody2D clone = (Rigidbody2D) Instantiate(enemy, new Vector3(), transform.rotation);
+        } else {
+            respawn.transform.position = new Vector3();
+            respawn.transform.rotation = new Quaternion();
+            respawn.SetActive(true);
+        }
     }
 }
